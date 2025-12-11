dd6bf7d01604992e6f8d4796a55b43e11bbd7ce1
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2025-12-11 18:15:52 +0000

    Add gradient clipping to prevent NaN loss from gradient explosion

--- peer/trainer.py
+++ peer/trainer.py
@@ -29,6 +29,8 @@ def train(model, train_loader, optimizer, device, scaler=None):
                 loss = F.cross_entropy(outputs, targets, ignore_index=0)
 
             scaler.scale(loss).backward()
+            scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             scaler.step(optimizer)
             scaler.update()
         else:
